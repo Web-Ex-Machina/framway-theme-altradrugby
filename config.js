280b125013c8481d3c615efdea12589399079f93
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-06-14 18:32:16 +0200

    tweak some config vars

--- config.js
+++ config.js
@@ -1,1 +1,13 @@
-module.exports = {};
+module.exports = {
+	'spacing':'3em',
+	'container':{
+		'xl'  : '1320px', // 1320px
+		'lg'  : '1140px', // 1140px
+		// 'md'  : '960px',
+		// 'sm'  : '720px',
+		// 'xs'  : '540px',
+		// 'xxs' : '100%',
+	},
+	'radius': '20px',
+	'griditem-minwidth': '30ch',
+};
