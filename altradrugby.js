034fc2595908d1e416a55725290a38497856916a
SeptimusVII <vansteene.quentin@gmail.com>
2025-07-01 18:47:50 +0200

    add getPercent function for simplePie Charts

--- altradrugby.js
+++ altradrugby.js
@@ -8,3 +8,12 @@ window.addEventListener("load", function(e) {
         }
     }
 });
+
+
+window.tooltip_getPercent = (tooltipItems) => {
+    console.log(tooltipItems);
+    let sumData = tooltipItems[0].dataset.data.reduce((a,c)=>a+c,0)
+    let currentValue = tooltipItems[0].dataset.data[tooltipItems[0].dataIndex];
+    let percent = (currentValue / sumData * 100).toFixed(2);
+    return percent+ '%';
+}
